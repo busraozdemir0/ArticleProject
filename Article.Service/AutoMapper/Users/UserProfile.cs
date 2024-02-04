f523f9adb2c49c33b1a5a3d032ab22c81c96114e
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-02-04 21:48:54 +0300

    User'ları rolleriyle birlikte listeleme - 27

---
+++ Article.Service/AutoMapper/Users/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using Article.Entity.DTOs.Users;
+using Article.Entity.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Article.Service.AutoMapper.Users
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<AppUser, UserDto>().ReverseMap();
+        }
+    }
+}
---
+++ Article.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Article.Entity.DTOs.Users;
+using Article.Entity.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Article.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly IMapper mapper;
+
+        public UserController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            this.userManager = userManager;
+            this.mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+            var map = mapper.Map<List<UserDto>>(users);
+
+            foreach (var item in map) // maplenmis user'lar uzerinde gez
+            {
+                var findUser=await userManager.FindByIdAsync(item.Id.ToString());  // user'in id'sini bul
+                var role = string.Join("",await userManager.GetRolesAsync(findUser));  // kullanicinin rolünü bul
+                                     //bu separator roller arasina koyacagi karakteri ifade eder
+                item.Role = role;
+            }
+
+            return View(map);
+        }
+    }
+}
